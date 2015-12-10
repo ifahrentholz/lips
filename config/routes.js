ae26bdc549d14a51f013e8d8066ebe113ea246ca
ifahrentholz <ingof.work@gmail.com>
2015-12-10 13:01:24 +0100

    start revision 1.0.0

--- config/routes.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-module.exports = function(app){
-  fs.readdirSync(App.appPath('app/controllers')).forEach(function(file) {
-    if (file == "setup.js") return;
-    var name = file.substr(0, file.indexOf('.'));
-    require(App.appPath('app/controllers') + '/' + name)(app);
-  });
-}
