e614cdb18a6086020cf4696d1f094a219fe6a529
ifahrentholz <ifahrentholz@pro-vision.de>
2016-11-02 15:12:20 +0100

    Refactorings for 4.0.0

--- backup/lib/index.js
+++
@@ -1,9 +0,0 @@
-var App = require("./config/app").App;
-var extend = require("node.extend");
-
-module.exports = function (options) {
-  App.settings = extend({}, App.settings, options);
-  App.start();
-};
-
-
